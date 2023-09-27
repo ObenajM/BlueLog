48cdc0b0210b66850f4ba4a13bf335076580265b
Obeimar Benavides <obenavides@unal.edu.co>
2023-09-27 16:14:19 -0500

    Ejemplo documentación ESP

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include <Wire.h>
 
 void K_4LD_init(void); // function prototype
-void K_4LD_init(void); // function prototype
-
-void K_4LD_init(void); // function prototype
 char* K_4LD_read(void); // function prototype
 
 void K_4LD_init(void); // function prototype
@@ -125,3 +122,42 @@ char* K_4LD_read(void)
   pad=&box_4LD[0];
   return pad;
 }
+
+
+
+//
+//#define I2C_DEV_ADDR 0x55
+//
+//uint32_t i = 0;
+//
+//void onRequest(){
+//  Wire.print(i++);
+//  Wire.print(" Packets.");
+//  Serial.println("onRequest");
+//}
+//
+//void onReceive(int len){
+//  Serial.printf("onReceive[%d]: ", len);
+//  while(Wire.available()){
+//    Serial.write(Wire.read());
+//  }
+//  Serial.println();
+//}
+//
+//void setup() {
+//  Serial.begin(115200);
+//  Serial.setDebugOutput(true);
+//  Wire.onReceive(onReceive);
+//  Wire.onRequest(onRequest);
+//  Wire.begin((uint8_t)I2C_DEV_ADDR);
+//
+//#if CONFIG_IDF_TARGET_ESP32
+//  char message[64];
+//  snprintf(message, 64, "%u Packets.", i++);
+//  Wire.slaveWrite((uint8_t *)message, strlen(message));
+//#endif
+//}
+//
+//void loop() {
+//
+//}
