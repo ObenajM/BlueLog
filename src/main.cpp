7a6a64d2aabb469cccdc9da7bd06d36ea98aa3a2
Obeimar Benavides <obenavides@unal.edu.co>
2023-09-27 12:04:21 -0500

    Primer codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,118 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-//int myFunction(int, int);
-#ifndef LED_BUILTIN
-  #define LED_BUILTIN 13
-#endif
+const int ledPin =  18;    // Led Rojo
+const int ledPin2 =  19;  // Led Azul CAmbio a pin analogo_a3
 
-void setup() {
-  // put your setup code here, to run once:
- pinMode(LED_BUILTIN,OUTPUT);
+int EOC = 3;// corresponde al D3
+
+void setup()
+{
+// init Led's
+pinMode(ledPin, OUTPUT);  // rojo
+pinMode(ledPin2, OUTPUT); // azul 
+digitalWrite(ledPin,LOW);   
+digitalWrite(ledPin2,LOW); 
+//pin available 4LD
+pinMode(EOC, INPUT);      // sets the digital pin as output
+/***********************************************************/
+// Open serial communications and wait for port to open: 
+Serial.begin(9600); //normal
+/***********************************************************/
+//init I2C 
+K_4LD_init();// i2c init todo lo anterior
+/***********************************************************/   
 }
 
+/*************************--inicial loop---*******************************/  
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+void loop()
+{
+
+while(1){
+         // K_4LD_init();
+          delay(1000);
+          Serial.println(K_4LD_read());
+          digitalWrite(ledPin, !digitalRead(ledPin));
+        }
+   
 }
 
-// put function definitions here:
+//*******************************************************************************************************************************************////
+ /*************************************************************/ 
+ void K_4LD_init(void)
+{
+  Wire.begin();
+ Wire.beginTransmission(0x40); // transmit to device #208 (0xD0)  //direccion del A/D conver
+  Wire.write(0x80);//HEX
+  Wire.write(0xAC);//HEX             // sends value byte 
+  //if(Wire.endTransmission(true)==0){Serial.print("success");}
+  //delay(8);//8mseg
+  Serial.println(String(Wire.endTransmission(true)));
+}
+/***********************************/
+char* K_4LD_read(void)
+{
+  //Variables 
+  unsigned long Pressure=0,Temp=0;
+  unsigned char a,b,c,d,e;
+  int EOC = 7;// corresponde al A0 
+  int cont=0;
+  char box_4LD[40]="";
+  char box_PsrSea[10];
+  char box_TempSea[10];
+  char tab[]="\t";
+  char *pad;
+
+  K_4LD_init();
+  cont=0;
+  Pressure=0;
+  Temp=0;
+  
+  while(digitalRead(EOC)== LOW)
+    {
+    Wire.begin();
+    Wire.beginTransmission(0x40); //FUNCIONANDO
+    Wire.write(172); 
+    Wire.endTransmission(true);
+    digitalWrite(ledPin2, !digitalRead(ledPin2));
+    }
+
+  Wire.beginTransmission(0x40); // transmit to device #208 (0xD0)  //direccion del A/D conver
+  Wire.write(0x81);//HEX
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x40, 5, true);
+  
+  while(Wire.available())    // slave may send less than requested
+     { 
+    a = Wire.read(); // receive a byte as character
+    b = Wire.read(); // receive a byte as character
+    c = Wire.read(); // receive a byte as character
+    d = Wire.read(); // receive a byte as character
+    e = Wire.read(); // receive a byte as character
+     }
+   Wire.endTransmission();
+     
+  //Calculo profundidad en Bares
+  Pressure=(Pressure|b)<<8;
+  Pressure=(Pressure|c);
+  unsigned long Psr_aux =(Pressure-16384)*3.0;//en long 32bits
+  float PsrSea=(Psr_aux/32768.0);// en float para coger las decimas
+  dtostrf(PsrSea,7,6,box_PsrSea);
+
+  //Calculo Temperatura
+  Temp=(Temp|d)<<8;
+  Temp=(Temp|e);
+  unsigned long  int Temp_aux=(Temp/16);// en float para coger las decimas
+  float TempSea=((Temp_aux-24)*0.05)-50;
+  dtostrf(TempSea,7,6,box_TempSea);
+
+  sprintf(box_4LD,box_TempSea);
+  strcat (box_4LD,tab);
+  strcat (box_4LD,box_PsrSea);
+
+
+  pad=&box_4LD[0];
+  return pad;
+
+}
