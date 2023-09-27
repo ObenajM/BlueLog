9fd80c53d59689545b90306b747bdbfbba0d5efc
Obeimar Benavides <obenavides@unal.edu.co>
2023-09-27 11:21:31 -0500

    xx

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@
 void setup() {
   // put your setup code here, to run once:
  pinMode(LED_BUILTIN,OUTPUT);
+ pinMode(LED_BUILTIN,OUTPUT);pinMode(LED_BUILTIN,OUTPUT);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
